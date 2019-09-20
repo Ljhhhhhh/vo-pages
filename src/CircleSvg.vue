cd31340e81dc42bf91ef0425f4338c647f34fb53
ljh666 <13216698987@163.com>
2019-09-20 14:33:04 +0800

    初始化下拉图标组件

---
+++ src/CircleSvg.vue
@@ -0,0 +1,16 @@
+<template>
+<div>this is circle svg</div>
+</template>
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue,
+} from 'vue-property-decorator';
+@Component
+export default class CircleSvg extends Vue {
+
+}
+</script>
+<style lang="scss" scoped>
+</style>
